56c104e37eb82ccdfb1540e0d592d507acbf135c
crongle <gasmazk73@gmail.com>
2020-08-29 12:56:07 +0100

    Updates

---
+++ precache-manifest.572926f7a94ec985b5769038677878c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8445797cfbf8b45ad71a30443974c6",
+    "url": "/tricky-typer/index.html"
+  },
+  {
+    "revision": "b765653628a12a0b8e3d",
+    "url": "/tricky-typer/static/css/main.e1c8a331.chunk.css"
+  },
+  {
+    "revision": "378f1a7dc487666ae2df",
+    "url": "/tricky-typer/static/js/2.4e4fccf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tricky-typer/static/js/2.4e4fccf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b765653628a12a0b8e3d",
+    "url": "/tricky-typer/static/js/main.df018202.chunk.js"
+  },
+  {
+    "revision": "10e46a9044250aebe4d1",
+    "url": "/tricky-typer/static/js/runtime-main.506bbf73.js"
+  }
+]);
